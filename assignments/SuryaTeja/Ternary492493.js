9a04151170491d679b0f3e3cc6c8382f15b08a8a
unknown <surya.teja98.nllr@gmail.com>
2019-08-13 13:22:37 +0530

    added ind. assns.

---
+++ assignments/SuryaTeja/Ternary492493.js
@@ -0,0 +1,14 @@
+var a = null;
+var b = null;
+// Using a ternary operator to check for null.
+b = a ? a : 0;
+console.log('b = a ? a : 0; // b = ' + b);
+myVal = 5;
+b = a ? a : 0;
+console.log('b = a ? a : 0; // b = ' + b);
+// Using a ternary operator to evaluate.
+var result = 5;
+var myVal = 5;
+var result1;
+result1 = (myVal == result) ? 'myVal and result both equal ' : 'values are not equal';
+console.log(result1);
---
+++ assignments/SuryaTeja/Ternary492493.ts
@@ -0,0 +1,17 @@
+var a: number = null;
+var b: number = null;
+
+// Using a ternary operator to check for null.
+b = a ? a : 0;
+console.log('b = a ? a : 0; // b = ' + b);
+
+myVal = 5;
+    b = a ? a : 0;
+    console.log('b = a ? a : 0; // b = ' + b);
+    
+// Using a ternary operator to evaluate.
+var result:number=5;
+var myVal:number=5;
+var result1:string;
+result1 = (myVal == result) ? 'myVal and result both equal ' : 'values are not equal';
+console.log(result1);
