a06c406769d4cd3e3a40861d8fd0a8f6727b91b2
John <john.bonda7@gmail.com>
2019-08-17 16:55:21 +0530

    pushed assignments

---
+++ assignments/John/style demo/styling.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-styling',
+  templateUrl: './styling.component.html',
+  styleUrls: ['./styling.component.css']
+})
+export class StylingComponent implements OnInit {
+
+  constructor() { }
+
+  
+  prop={
+    color: "white"
+  };
+
+  ngOnInit() {
+  }
+
+}
